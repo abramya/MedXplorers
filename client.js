93c3b3506570ca5aab542be5d5d7c3397211f315
abramya <159501514+abramya@users.noreply.github.com>
2024-09-12 20:46:08 +0530

    Create client.js

---
+++ client.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import Dashboard from './components/Dashboard';
+import './styles.css';
+
+function App() {
+  const [healthData, setHealthData] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/health')
+      .then((res) => res.json())
+      .then((data) => setHealthData(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Welcome to MedXplorers</h1>
+      <Dashboard healthData={healthData} />
+    </div>
+  );
+}
+
+export default App;
