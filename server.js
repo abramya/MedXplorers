9f311ad11cec8b353dd696440ec50845a4f573f8
abramya <159501514+abramya@users.noreply.github.com>
2024-09-12 20:51:43 +0530

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+ 
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/medxplorers', {
   useNewUrlParser: true,
